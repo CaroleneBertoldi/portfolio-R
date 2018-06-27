c93cf3465a06af575ed8ea5f739ae9bb00bb4def
Carolene Bertoldi <projeto.bot.mapfre@gmail.com>
2018-06-26 23:50:29 -0300

    Adicionando htmls a templete

---
+++ src/ui.R
@@ -0,0 +1,59 @@
+#install.packages("shinydashboard")
+library(shinydashboard)
+
+sidebar = dashboardSidebar(
+  sidebarMenu(
+    menuItem("Capa", 
+             tabName="home", 
+             icon=icon("home")
+    ),
+    menuItem("Sobre o aluno", 
+             tabName="aluno",
+             icon = icon("users")
+    ),
+    hr(),
+    menuItem("Conceitos Básicos", 
+             icon = icon('line-chart'), 
+             tabName="aula01"
+    ),
+    menuItem("Aula 02", 
+             icon = icon('line-chart'),
+             tabName="aula02-01"
+    ),
+    menuItem("Aula 03", 
+             icon = icon('line-chart'),
+             tabName="aula03-01"
+    )
+  )
+)
+
+body = dashboardBody(
+  tabItems(
+    tabItem(tabName = "home",
+            h2("PÃ¡gina inicial"),
+            h4("Fazer uma capa bonitinha ..."),
+            h5("O que eu aprendi nas aulas de R")
+    ),
+    tabItem(tabName = "aluno",
+            h4("Falar um pouco sobre o aluno / Grupo")
+    ),
+    tabItem(tabName = "aula01",
+            includeHTML("Aula01.html")
+    ),
+    tabItem(tabName = "aula02-01",
+            includeHTML("Aula02.html")
+    ),
+    tabItem(tabName = "aula03-01",
+            includeHTML("Aula03.html")
+    )
+  )
+)
+
+dashboardPage(header = dashboardHeader(title = "Portfólio R"),
+              skin = "purple",
+              sidebar = sidebar,
+              body = body)
+
+
+
+
